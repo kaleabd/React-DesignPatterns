ad4933c54347d208b504614b5f9fad402ce6b3c1
kaleabd <kaleabdereje94@gmail.com>
2023-06-15 09:29:43 +0300

    identation fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,32 @@
-import SplitScreen from './Layout-Components/Split-Screen/SplitScreen'
+import SplitScreen from "./Layout-Components/Split-Screen/SplitScreen";
 
 const RightHandComponent: React.FC<{ message: string }> = ({ message }) => {
   return <h1>{message}</h1>;
 };
 
-
-const LeftHandComponent: React.FC<{ userName: string }> = ({userName}) => {
-  return <h1>{userName}</h1>
-}
+const LeftHandComponent: React.FC<{ userName: string }> = ({ userName }) => {
+  return <h1>{userName}</h1>;
+};
 
 const App = () => {
   return (
     <div>
-{/* The code is rendering a component called `SplitScreen` with two child components,
-`LeftHandComponent` and `RightHandComponent`. The `leftColor` and `rightColor` props are setting the
-background color of the left and right components respectively. */}
-      {/* <SplitScreen 
-        left={LeftHandComponent}
-        leftColor='bg-blue-400'
-        right={RightHandComponent}
-        rightColor='bg-green-400'
-      /> */}
-        <SplitScreen leftColor='bg-blue-400'  rightColor='bg-green-400'>
-          <LeftHandComponent userName='kaleab'/>
-          <RightHandComponent message="hello world!"/>
-        </SplitScreen>
+      {/* The code is rendering a component called `SplitScreen` with two child components,
+        `LeftHandComponent` and `RightHandComponent`. The `leftColor` and `rightColor` props are setting the
+        background color of the left and right components respectively.
+        <SplitScreen 
+          left={LeftHandComponent}
+          leftColor='bg-blue-400'
+          right={RightHandComponent}
+          rightColor='bg-green-400'
+        /> 
+      */}
+      <SplitScreen leftColor="bg-blue-400" rightColor="bg-green-400">
+        <LeftHandComponent userName="kaleab" />
+        <RightHandComponent message="hello world!" />
+      </SplitScreen>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
