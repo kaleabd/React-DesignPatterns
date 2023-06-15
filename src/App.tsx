d3d207999b1dcb0e9a37f224a7005bb55c01edaa
kaleabd <kaleabdereje94@gmail.com>
2023-06-15 08:57:17 +0300

    split screen components

--- src/App.tsx
+++ src/App.tsx
@@ -1,73 +1,23 @@
-import SplitScreen from "./SplitScreen";
-import Right from "./components/Right";
-import Left from "./components/Left";
-import RegularList from "./RegularList";
-import { people } from "./assets/people";
-import SmallPersonListItem from "./people/SmallPersonListItem";
-import LargePersonListItem from "./people/LargePersonListItem";
-import { products } from "./assets/products";
-import SmallProductListItem from "./product/SmallProductItem";
-import LargeProductListItem from "./product/LargeProductItem";
-import NumberedList from "./NumberedList";
-import Modal from "./Modal";
-import CurrentUserLoader from "./Container-component/CurrentUserLoader";
-import UserInfo from "./Container-component/UserInfo";
+import React from 'react'
+import SplitScreen from './Layout-Components/Split-Screen/SplitScreen'
+
+const RightHandComponent = () => {
+  return <h1>Right hand!</h1>
+}
+
+const LeftHandComponent = () => {
+  return <h1>Left hand!</h1>
+}
 
 const App = () => {
   return (
-    <>
-      <SplitScreen leftWeight={1} rightWeight={2}>
-        <Left message="Hello" />
-        <Right name="Kaleab!" />
-      </SplitScreen>
-      {/* LIST AND LIST ITEMS */}
-      {/* `<RegularList>` is rendering a list of items from the `people` array, where each item is
-      represented by a `SmallPersonListItem` component. The `resourceName` prop is used to specify
-      the name of the resource being listed (in this case, "person"). */}
-      <RegularList
-        items={people}
-        resourceName="person"
-        itemComponent={SmallPersonListItem}
+    <div>
+      <SplitScreen 
+        left={LeftHandComponent}
+        right={RightHandComponent}
       />
-      <RegularList
-        items={people}
-        resourceName="person"
-        itemComponent={LargePersonListItem}
-      />
-      <br />
-      <RegularList
-        items={products}
-        resourceName="product"
-        itemComponent={SmallProductListItem}
-      />
-      <br />
-      <RegularList
-        items={products}
-        resourceName="product"
-        itemComponent={LargeProductListItem}
-      />
-      <br />
-      <NumberedList
-      items={products}
-      resourceName="product"
-      itemComponent={SmallProductListItem}
-      />
-      <br />
-      <Modal>
-        <LargeProductListItem product={products[0]}/>
-      </Modal>
-      <br />
-      <br />
-      {/* `<CurrentUserLoader>` is a higher-order component that loads the current user's information
-      and passes it down to its child components as props. In this case, the child component is
-      `<UserInfo/>`, which likely displays information about the current user. */}
-      <CurrentUserLoader>
-        <UserInfo/>
-      </CurrentUserLoader>
-      <br />
-      <br />
-    </>
-  );
-};
+    </div>
+  )
+}
 
-export default App;
+export default App
--- src/Layout-Components/Split-Screen/SplitScreen.tsx
+++ src/Layout-Components/Split-Screen/SplitScreen.tsx
@@ -1,7 +1,18 @@
-const SplitScreen = () => {
+import React from "react"
+
+type SplitScreenProps = {
+  left: React.ComponentType
+  right: React.ComponentType
+}
+const SplitScreen: React.FC<SplitScreenProps> = ({left: Left, right: Right}) => {
   return (
-    <div>
-      
+    <div className="flex">
+      <div className="flex-1">
+        <Left />
+      </div>
+      <div className="flex-1">
+        <Right />
+      </div>
     </div>
   )
 }
